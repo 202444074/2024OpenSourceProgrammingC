e69fb47c0007dc45e5884892d5abdcff7a3bf624
davdav0309 <davdav0309@gmail.com>
2024-10-04 15:49:10 +0900

    v0.7 zero values

--- week06/main.go
+++ week06/main.go
@@ -2,20 +2,18 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
-func main() {
-	i := 13              //var i int = 13
-	var f float64 = 12.9 //f := 12.9
-	c1 := 'Z'            // 99
-	c2 := '김'            // 44608, unicode
+func main() { //v0.6 unicode 확인할 것
+	var f float64
+	var i int
+	var b bool
+	var s string
 
-	fmt.Printf("value i : %d, value f : %f\n", i, f)
-	//fmt.Printf("%d * %f = %f\n", i, f, i*f)   c언어와 다르게 go는 묵시적 형변환 안함
-	//fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f) //밑의 type를 보면 i가 일시적으로만 변한것
-	fmt.Printf("%d * %f = %d\n", i, f, i*int(f)) //i와 f가 int지만 %f에서 오류가 나서 %d로 바꿈
-	fmt.Println(c1, c2)
-	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f), reflect.TypeOf(c1), reflect.TypeOf(c2))
-
+	fmt.Println(f, b, s, i)
+	fmt.Printf("%f%t%s%d\n", f, b, s, i) // zero values
+	f = 2.7
+	i = 3
+	fmt.Print("\n\n", f <= float64(i), "\n") // true false로 나오는 것이 bool
+	// go build main.go -> main.exe 생성 -> ./main.exe -> week06 main.go 실행
 }
