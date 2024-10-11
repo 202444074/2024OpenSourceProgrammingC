fc5c6434a221d630f25c2b95b55620af867f25e8
davdav0309 <davdav0309@gmail.com>
2024-10-11 15:27:44 +0900

    v1.3 Parse

--- week07/main.go
+++ week07/main.go
@@ -5,17 +5,24 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
+	fmt.Print("input your score : ")
 	in := bufio.NewReader(os.Stdin)
-	fmt.Print("input your name : ")
-	name, err := in.ReadString('\n')
-	// fmt.Println(i, err)  _ 언더스코어로 err를 안 보이게 처리해서 출력할 때도 안 보이게 할 수 있다.
-
+	i, err := in.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
+	}
+	i = strings.TrimSpace(i)                // 줄바꿈등을 제거(줄바꿈 \n이 에러를 발생), 파이썬의 strip과 비슷함
+	score, _ := strconv.ParseInt(i, 16, 32) // 정수형으로 변환 16진수 32bit
+	if score >= 60 {
+		fmt.Println("A")
+		fmt.Printf("%d\n", score)
 	} else {
-		fmt.Println(name)
+		fmt.Println("BCDF")
+		fmt.Printf("%d\n", score)
 	}
 }
