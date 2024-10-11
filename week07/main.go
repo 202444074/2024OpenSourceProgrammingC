56a9d16ede3da50f6de31a6eafa813a2eac54ce6
davdav0309 <davdav0309@gmail.com>
2024-10-11 15:34:34 +0900

    v1.4 Scope

--- week07/main.go
+++ week07/main.go
@@ -10,19 +10,19 @@ import (
 )
 
 func main() {
-	fmt.Print("input your score : ")
+	fmt.Print("점수 입력 : ")
 	in := bufio.NewReader(os.Stdin)
 	i, err := in.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
 	}
 	i = strings.TrimSpace(i)                // 줄바꿈등을 제거(줄바꿈 \n이 에러를 발생), 파이썬의 strip과 비슷함
-	score, _ := strconv.ParseInt(i, 16, 32) // 정수형으로 변환 16진수 32bit
-	if score >= 60 {
-		fmt.Println("A")
-		fmt.Printf("%d\n", score)
+	score, _ := strconv.ParseInt(i, 10, 32) // 정수형으로 변환 10진수 32bit
+	var grade string
+	if score >= 90 {
+		grade = "A" // 위에서 grade 전역 변수를 정의 해줘서 :=가 아닌 =로 바로 넣으면 됨
 	} else {
-		fmt.Println("BCDF")
-		fmt.Printf("%d\n", score)
+		grade = "BCDF"
 	}
+	fmt.Printf("%d점은 %s등급 입니다.\n", score, grade)
 }
