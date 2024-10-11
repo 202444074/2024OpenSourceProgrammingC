4bc31378dbd82855bf0b1fd9100eeb8a48e74d3b
davdav0309 <davdav0309@gmail.com>
2024-10-11 14:33:24 +0900

    v1.2 If

--- week07/main.go
+++ week07/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -11,6 +12,10 @@ func main() {
 	fmt.Print("input your name : ")
 	name, err := in.ReadString('\n')
 	// fmt.Println(i, err)  _ 언더스코어로 err를 안 보이게 처리해서 출력할 때도 안 보이게 할 수 있다.
-	fmt.Println(name)
-	fmt.Println(err)
+
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println(name)
+	}
 }
