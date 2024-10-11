c0b38a32b66c736b5366972c60551560634c91a7
davdav0309 <davdav0309@gmail.com>
2024-10-11 13:54:17 +0900

    v0.8 Time

---
+++ week07/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	var now time.Time = time.Now()
+	var month int = int(now.Month())
+	//	var month time.Month = now.Month()
+	fmt.Println(month)
+}
