891fa6f4ef8616c6c6434a3fab6664d16ded6671
davdav0309 <davdav0309@gmail.com>
2024-11-01 15:22:22 +0900

    v2.7 Prime number

--- week10/main.go
+++ week10/main.go
@@ -25,11 +25,15 @@ func main() {
 	}
 
 	var isPrime bool = true
-	if n <= 1 { // A prime number has only one of the natural numbers greater than 1 and itself as a divisor.
+	if n <= 1 {
+		isPrime = false
+	} else if n == 2 {
+		isPrime = true
+	} else if n%2 == 0 { // 2를 제외한 짝수는 모두 소수가 아님
 		isPrime = false
 	} else {
 		j := 2
-		for j <= int(math.Sqrt(float64(n))) { // n이 일시적으로 float로 바뀌고 이를 다시 int로 바꿈, int로 바꾸면서 나머지를 버리게 되니까 <=으로 바꿈
+		for j <= int(math.Sqrt(float64(n))) {
 			if n%j == 0 {
 				isPrime = false
 				break
