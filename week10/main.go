a374740b97f5fd44c8733e2e2eae26cb1a81b230
davdav0309 <davdav0309@gmail.com>
2024-11-01 15:36:59 +0900

    v3.0 Prime number

--- week10/main.go
+++ week10/main.go
@@ -9,8 +9,33 @@ import (
 	"strings"
 )
 
+func isPrime(n int) bool {
+	// var isPrime bool = true / 리턴으로 true false 받을 거니까
+	if n <= 1 {
+		//isPrime = false
+		return false
+	} else if n == 2 {
+		//isPrime = true
+		return true
+	} else if n%2 == 0 {
+		//isPrime = false
+		return false
+	} else {
+		j := 3
+		for j*j <= n {
+			if n%j == 0 {
+				// isPrime = false
+				// break
+				return false
+			}
+			fmt.Printf("%d ", j)
+			j += 2
+		}
+	}
+	return true // 모든 if문을 벗어난 소수임
+}
+
 func main() {
-	//fmt.Printf("%f\n", math.Sqrt(19.0))   sqrt는 제곱근이라는 뜻
 	fmt.Print("input number : ")
 	in := bufio.NewReader(os.Stdin)
 	i, err := in.ReadString('\n')
@@ -22,27 +47,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	var isPrime bool = true
-	if n <= 1 {
-		isPrime = false
-	} else if n == 2 {
-		isPrime = true
-	} else if n%2 == 0 {
-		isPrime = false
-	} else {
-		j := 3
-		//for j <= int(math.Sqrt(float64(n))) {
-		for j*j <= n { // 제곱근이니까 이와 같이 간결하게 바꾸면 간편하게 보임
-			if n%j == 0 {
-				isPrime = false
-				break
-			}
-			fmt.Printf("%d ", j)
-			j += 2
-		}
-	}
-	if isPrime {
+	if isPrime(n) {
 		fmt.Printf("%d is prime number.", n)
 	} else {
 		fmt.Printf("%d is NOT prime number.", n)
