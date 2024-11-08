2261bcef92371adb6d93e97d5d868628aa9c33d8
davdav0309 <davdav0309@gmail.com>
2024-11-08 14:40:14 +0900

    v3.5 Package

--- week11/main.go
+++ week11/main.go
@@ -2,7 +2,8 @@ package main
 
 import "week11/greeting"
 
-func main() { // greeting을 치면 경로가 보임, .을 찍으면 쓸 수 있는 함수가 보임
-	greeting.Hello("Inha")
-	greeting.Hi("Havard")
+func main() {
+	//greeting.Hello("Inha")
+	//greeting.Hi("Havard")
+	greeting.EnglishGreetings("Inha")
 }
