2376004fc07d90520a7e56e6525e1e9f75a1793f
davdav0309 <davdav0309@gmail.com>
2024-11-08 14:25:33 +0900

    v3.3 Prime number

--- week11/main.go
+++ week11/main.go
@@ -29,25 +29,33 @@ func isPrime(n int) bool {
 	return true
 }
 
-func getInteger() int {
+func getInteger() (int, error) {
 	in := bufio.NewReader(os.Stdin)
 	a, err := in.ReadString('\n')
 	if err != nil {
-		log.Fatal(err)
+		//log.Fatal(err)
+		return 0, err
 	}
 	a = strings.TrimSpace(a)
 	n, err := strconv.Atoi(a)
 	if err != nil {
-		log.Fatal(err)
+		//log.Fatal(err)  이 코드는 에러를 검출하면 바로 코드를 중단시키고 에러를 출력함
+		return 0, err // 이 코드는 에러를 리턴해서 메인에서 에러를 처리함
 	}
-	return n
+	return n, nil
 }
 
 func main() {
 	fmt.Print("input start number : ")
-	n1 := getInteger()
+	n1, err := getInteger()
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Print("input end number : ")
-	n2 := getInteger()
+	n2, err := getInteger()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	for j := n1; j <= n2; j++ {
 		if isPrime(j) {
