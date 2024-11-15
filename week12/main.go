3d5a7381d15669ffb5ab01475c0274cd2119a074
davdav0309 <davdav0309@gmail.com>
2024-11-15 15:22:31 +0900

    v3.9 Array

--- week12/main.go
+++ week12/main.go
@@ -2,37 +2,17 @@ package main
 
 import (
 	"fmt"
-	"time"
+
+	"github.com/headfirstgo/keyboard"
 )
 
 func main() {
-	var scores [3]int
-	scores[1] = 90
-	// fmt.Println(scores[1], scores[0]) // , scores[3]) 인덱스 범위를 벗어나서 오류 발생
-	// fmt.Printf("%#v\n", scores)       // %#v는 배열 리터럴 형태로 보여줌
-	// fmt.Println(scores)               // 이거는 그냥 배열이 출력
-
-	for i := 0; i < len(scores); i++ { // <= 를 쓰면 0부터 3까지 4개의 인덱스를 찾기 때문에 4번쨰 런타임에 에러 발생
-		fmt.Printf("%d ", scores[i])
+	var gpas [3]float64 // github.com/headfirstgo/keyboard 에서 가져올 함수에서 float를 리턴해서 float64로 만듬
+	for i := 0; i < len(gpas); i++ {
+		fmt.Print("Input float number : ")
+		gpas[i], _ = keyboard.GetFloat() // float와 err가 들어가야하는데 에러가 안 난다고 가정하고 _를 넣음
 	}
-
-	// var dates [3]time.Time
-	// dates[1] = time.Unix(194720000, 0)
-	// fmt.Println(dates[1])
-
-	// var dates [3]time.Time = [3]time.Time{time.Unix(0, 0), time.Unix(1, 0), time.Unix(1947200001, 0)}
-	// fmt.Println(dates[0], dates[1], dates[2])
-
-	dates := [3]time.Time{time.Unix(0, 0), time.Unix(1, 0), time.Unix(1947200001, 0)} // := 사용 가능
-	// fmt.Println(dates[0], dates[1], dates[2])
-	// fmt.Printf("%#v\n", dates)
-	// fmt.Println(dates)
-
-	fmt.Println('\n')
-	// for i, date := range dates {
-	// 	fmt.Println(i, date)
-	// }
-	for _, date := range dates { // 파이선 스타일로 만든 안전한 방법
-		fmt.Println(date)
+	for _, gpa := range gpas {
+		fmt.Println(gpa)
 	}
 }
