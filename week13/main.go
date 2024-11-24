342f3ebd24f4ba87b076579150ab83b22351856c
davdav0309 <davdav0309@gmail.com>
2024-11-24 20:32:55 +0900

    v4.4 Slice

--- week13/main.go
+++ week13/main.go
@@ -1,18 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	var emptySlice []bool                               // bool로 만든 배열에는 true, false 만 들어갈 수 있는 배열임
-	fmt.Printf("%#v %d\n", emptySlice, len(emptySlice)) // slice zero value (nil), 0
-	if len(emptySlice) == 0 {
-		emptySlice = append(emptySlice, true)
+	//fmt.Println(os.Args[1:], len(os.Args))
+	slices := os.Args[1:] //는 첫번째 프로그램 이름을 제외한 나머지 인자를 가져온다는 뜻
+	fmt.Println(slices[1])
+	for _, slice := range slices {
+		fmt.Println(slice)
 	}
-	fmt.Printf("%#v %d\n", emptySlice, len(emptySlice)) // []bool{true}, 1
-
-	var gpa [5]float64 = [5]float64{3.5, 4.1, 4.5, 3.9, 4.23}
-	gpa_slice := gpa[1:4]
-	gpa[2] = 2.71
-	gpa_slice = append(gpa_slice, 4.3, 5.55)
-	fmt.Println(len(gpa_slice), gpa_slice, gpa)
+	slices = append(slices, "forever", "!")
+	fmt.Println(slices, len(slices))
 }
