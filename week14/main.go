5ae8185059ac1e6c903f9d351373e6090537d32d
davdav0309 <davdav0309@gmail.com>
2024-11-29 14:51:04 +0900

    v4.9 Struct

--- week14/main.go
+++ week14/main.go
@@ -4,21 +4,19 @@ import (
 	"fmt"
 )
 
+type student struct { // 코드가 중복되는 것을 type로 바꿀 수 있음
+	id   int
+	name string
+	gpa  float32
+}
+
 func main() {
-	var student struct {
-		id   int
-		name string
-		gpa  float32
-	}
-	student.id = 202444074
-	student.name = "이영희"
-	student.gpa = 4.5
-	fmt.Println(student.gpa)
-	var student2 struct {
-		id   int
-		name string
-		gpa  float32
-	}
+	var student1 student
+	student1.id = 202444074
+	student1.name = "이영희"
+	student1.gpa = 4.5
+	fmt.Println(student1.gpa)
+	var student2 student
 	student2.id = 20241234
 	student2.name = "아이다"
 	student2.gpa = 4.43
